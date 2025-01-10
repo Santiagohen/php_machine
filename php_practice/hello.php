841445312f03f1eb443e0b5d476a135b8c21acd3
SantiagoHe <128274753+Santiagohen@users.noreply.github.com>
2025-01-10 00:30:00 +0000

    s

---
+++ php_practice/hello.php
@@ -0,0 +1,8 @@
+<?php
+$myName = "jeb";
+$myFriend = "Leo";
+echo "hello, world! \n";
+
+echo "my name is $myName my friends name is $myFriend \n";
+echo "hello $myFriend \n";
+echo "Im so smart" . "Im smart";

a6981dfb1d113cddc562a4fad4e25ac35c45b8f0
SantiagoHe <128274753+Santiagohen@users.noreply.github.com>
2025-01-10 23:16:02 +0000

    started bubblesort

---
+++ php_practice/algorithms/bubbleSort.php
@@ -0,0 +1,19 @@
+<?php
+
+
+function bubbleSort (array $arr):array{
+   $n = count($arr); 
+       #  echo "The length of $arr is $n";
+   for($i = 0; $i <= $n -1; $i++ ){
+ echo "$arr[$i], ";
+   }
+    return $arr;
+}
+function optimizeBubbleSort(array $arr):array{
+    $n = count($arr)
+        for($i )
+}
+
+
+$arr = [1,2,3,4,5];
+bubbleSort($arr);
